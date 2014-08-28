8a567e729dd641b0406c0a7a55551f4f3e3de4ef
idooo <alex.shteinikov@gmail.com>
2014-08-28 15:28:21 +1000

    experiment 1

--- src/blackmask.js
+++ src/blackmask.js
@@ -8,119 +8,78 @@
     'use strict';
 
     angular.module('blackMask', [])
-        .directive('bmNumbersMask', function () {
+        .directive('bmNumbersMask', function ($timeout) {
 
             return {
                 restrict: 'A',
-                replace: false,
+                replace: true,
                 scope: {
                     maxLength: '=',
                     placeholder: '@'
                 },
                 require: '?ngModel',
-                link: function (scope, element, attrs, ctrl) {
-                    if (!ctrl) return;
-                    
-                    var placeholderChar = '_';
-                    if (typeof scope.placeholder !== 'undefined') {
-                        placeholderChar = scope.placeholder;
+                template: [
+                        '<div class="ng-bm-element">',
+                            '<div class="ng-bm-container"></div>',
+                            '<input type="hidden">',
+                        '</div>'
+                    ].join(' '),
+                compile: function compile(tElement, tAttrs, transclude) {
+
+                    var container = tElement.find('div');
+
+                    for (var i=0; i<9; i++) {
+                        container.append('<input type="text" maxLength="1">');        
                     }
 
-                    var indexOf = function(str) {
-                        for (var i=0; i<str.length; i++) {
-                            if (str[i] === placeholderChar) return i;
-                        }
-                        return -1;
-                    };
+                    return function link(scope, iElement, iAttrs) { 
+                        var inputs = container.find('input')
+                        inputs.on('keyup', function(e) {
+                            var current = this,
+                                key = e.keyCode || e.which,
+                                prev = this.previousSibling,
+                                next = this.nextSibling;
 
-                    var moveCaretToPosition = function(el, position) {
-                        if (typeof position === 'undefined') {
-                            position = 0;
-                        }
-                        if (typeof el.selectionStart === "number") {
-                            el.selectionStart = el.selectionEnd = position;
-                        } else if (typeof el.createTextRange !== "undefined") {
-                            var range = el.createTextRange();
-                            range.collapse(true);
-                            range.moveEnd('character', position);
-                            range.moveStart('character', position);
-                            range.select();
-                        }
-                    };
+                            if (key === 9) return current.focus();
+                            if (key === 37) return prev.focus();
+                            if (key === 39) return next.focus();
 
-                    var getCaretPosition = function (el) {
-                        var caretPosition = 0,
-                            placeholderStart = indexOf(el.value);
-                        // IE Support
-                        if (document.selection) {
-                            el.focus();
-                            var range = document.selection.createRange();
-                            range.moveStart('character', -el.value.length);
-                            caretPosition = range.text.length;
-                        }
-                        else if (el.selectionStart || parseInt(el.selectionStart, 10) === 0) {
-                            caretPosition = el.selectionStart;
-                        }
-                        if (placeholderStart !== -1 && caretPosition > placeholderStart) {
-                            caretPosition = placeholderStart;
-                        }
-                        return caretPosition;
-                    };
-
-                    var placeholder = '';
-                    for (var i=0; i<scope.maxLength; i++) {
-                        placeholder += placeholderChar;
-                    }
-
-                    if (isNaN(ctrl.$modelValue) || ctrl.$modelValue.length === 0) {
-                        ctrl.$setViewValue(placeholder);
-                        ctrl.$render();
-                        moveCaretToPosition(element[0]);
-                    }
-
-                    element.on('keydown', function(e) {
-                        var key = e.keyCode || e.which,
-                            caretPosition = getCaretPosition(element[0]);
-
-                        if (key === 8) {
-                            scope.caretPosition = caretPosition - 1;
-                        }
-                        else if (key >= 48 && key <= 57) {
-                            scope.caretPosition = caretPosition + 1;
-                        }
-                        else {
-                            scope.caretPosition = caretPosition;
-                        }
-                        scope.lastKey = key;
-                    });
-
-                    ctrl.$parsers.push(function(value) {
-                        var formatedValue = '';
-                        if (!value) {
-                            formatedValue = placeholder;
-                        }
-                        else if (indexOf(value) === -1 && indexOf(ctrl.$modelValue) === -1 && indexOf(ctrl.$viewValue) === -1 && scope.lastKey !== 8 && scope.lastKey !== 46) {
-                            formatedValue = ctrl.$modelValue;
-                        }
-                        else {
-                            formatedValue = (value.replace(/\D/g, '') + placeholder).slice(0, scope.maxLength);
-                        }
-                        scope.lastKey = -1;
-
-                        if (ctrl.$viewValue !== formatedValue) {
-                            ctrl.$setViewValue(formatedValue);
-                            ctrl.$render();
-
-                            if (indexOf(formatedValue) > -1) {
-                                if (scope.caretPosition === 0) {
-                                    scope.caretPosition = indexOf(formatedValue);
-                                }
-                                moveCaretToPosition(element[0], scope.caretPosition);
-                                scope.caretPosition = 0;
+                            if (key === 8) {
+                                $timeout(function() {
+                                    current.value = '';
+                                    prev.focus()
+                                }, 10);
+                                return;
                             }
-                        }
-                        return formatedValue;
-                    });
+
+                            if (next && typeof next.focus !== "undefined") {
+                                $timeout(function() {
+                                    if (current.value.length >= 1) next.focus()
+                                }, 10)
+                            }
+                        });
+
+                        var pasteStr = function(str, input) {
+                            input.value = str[0];
+                            if (str.length > 1 && input.nextSibling) pasteStr(str.slice(1), input.nextSibling)
+                        };
+
+                        inputs.on('paste', function(e) {
+                            var pastedText = undefined,
+                                current = this;
+
+                            if (window.clipboardData && window.clipboardData.getData) { // IE
+                                pastedText = window.clipboardData.getData('Text');
+                            } else if (e.clipboardData && e.clipboardData.getData) {
+                                pastedText = e.clipboardData.getData('text/plain');
+                            }
+                            pasteStr(pastedText, this)
+                            $timeout(function() {
+                                current.focus();
+                            }, 100)
+                        })
+
+                    }
                 }
             };
         });
